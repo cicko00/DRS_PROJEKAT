21bc841b1ca9ea9e3bc062496e7494fbde7e3daf
Cvijetin Glišić <cilecickocvijetin@gmail.com>
2023-01-04 14:08:42 +0100

    Update login.component.ts

--- UI/src/app/login/login.component.ts
+++ UI/src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User_login } from '../models/user.model';
 import { NavigationServiceService } from '../services/navigation-service.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,33 +12,55 @@ import { NavigationServiceService } from '../services/navigation-service.service
 })
 export class LoginComponent implements OnInit {
 
-  errorMessage = 'none';
-  sucessMessage = 'none';
-  errorMsg = '';
+  users:User_login={
+    username: '',
+    password: ''
+  };
 
-  constructor(private nav: NavigationServiceService, private route: Router) { }
+  constructor(private nav: NavigationServiceService, private route: Router,private formbuilder:FormBuilder) { }
+
+
+  form=this.formbuilder.group(
+    {username:'',password:''}
+  );
+
+
+   sendLoginData(): void{
+    console.log(this.users.username);
+    this.users.username=this.form.value.username as string;
+    if(this.users.username.trim()==""){
+      window.alert("All fields are required!")
+      return;
+    }
+
+    this.users.password=this.form.value.password as string;
+    if(this.users.password.trim()==""){
+      window.alert("All fields are required!")
+      return;
+    }
+
+
+    this.nav.tryLogin(this.users).subscribe(s=>{
+      
+      if ((s as string) == "FALSE"){
+        window.alert("Invalid username or password!");
+        
+      }
+      else if((s as string)=="TRUE"){
+        window.alert("Succes!")
+        
+        this.route.navigate(["/home"]);
+        this.nav.showNoLogin();
+      }
+    });
+
+
+
+  }
 
   ngOnInit(){
-    this.nav.shownonLogin();
+    
   }
 
-  passVal($f: NgForm) {
-    console.log($f.value)
-    if (!$f.value.password || !$f.value.email) {
-      this.errorMessage = 'block';
-      this.sucessMessage = 'none';
-      this.errorMsg = 'Both Password and Username are required.!';
-    }
-    if ($f.value.password && $f.value.email) {
-      if ($f.value.email === 'test' && $f.value.password === 'test') {
-        this.errorMessage = 'none';
-        this.sucessMessage = 'block';
-        this.route.navigateByUrl('/home');
-      } else {
-        this.errorMessage = 'block';
-        this.sucessMessage = 'none';
-        this.errorMsg = 'Either Password or Username is incorrect.!';
-      }
-    }
-  }
+ 
 }
